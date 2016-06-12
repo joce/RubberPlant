3c1923f7b4d3818d1012ca119572e79f49b4598b
joce <jocelynlegault@gmail.com>
2016-06-12 13:29:12 -0400

    Consolidate extensions for atoms in single file
    Signed-off-by: joce <jocelynlegault@gmail.com>

---
+++ src/RubberPlant/LSystem/AtomExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RubberPlant
+{
+    static class AtomExtensions
+    {
+        public static string ToAtomString(this IEnumerable<Atom> atoms)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var atom in atoms)
+            {
+                sb.Append(atom.RuleName);
+            }
+
+            return sb.ToString();
+        }
+
+        public static List<Atom> ToAtoms(this string s)
+        {
+            return s.Select(c => new Atom(c)).ToList();
+        }
+
+        public static List<Atom> ToAtoms(this IEnumerable<char> s)
+        {
+            return s.Select(c => new Atom(c)).ToList();
+        }
+    }
+}
